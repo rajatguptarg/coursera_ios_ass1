c47ffd59a45f006262786d420c42bd624c433089
Rajat Gupta <rajat.gupta712@gmail.com>
2016-02-13 14:00:18 +0530

    Initial Commit

---
+++ assignment_01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  assignment_01
+//
+//  Created by Rajat Gupta on 13/02/16.
+//  Copyright © 2016 Rajat Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ assignment_01Tests/assignment_01Tests.m
@@ -0,0 +1,39 @@
+//
+//  assignment_01Tests.m
+//  assignment_01Tests
+//
+//  Created by Rajat Gupta on 13/02/16.
+//  Copyright © 2016 Rajat Gupta. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface assignment_01Tests : XCTestCase
+
+@end
+
+@implementation assignment_01Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
